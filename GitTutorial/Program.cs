9b7ec56a44b51e448ffee1682d6bf043790776a0
Eric Lin <eric.yijuin.lin@gmail.com>
2022-03-21 16:41:12 +0800

    modified in master branch

--- GitTutorial/Program.cs
+++ GitTutorial/Program.cs
@@ -4,3 +4,4 @@ Console.WriteLine("Hello, Another World");
 Console.WriteLine("Modified in Folder B");
 Console.WriteLine("Modified in Folder A");
 Console.WriteLine("Keep going");
+Console.WriteLine("Modified in master branch");
