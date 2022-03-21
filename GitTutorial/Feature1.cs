af6f84fb6cdb70562465609e0f3b8fb48f5106b6
Eric Lin <eric.yijuin.lin@gmail.com>
2022-03-21 16:19:54 +0800

    feature_1 commit 1

---
+++ GitTutorial/Feature1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTutorial
+{
+    public class Feature1
+    {
+        public static void Feature1Method()
+        {
+            Console.WriteLine("This is feature 1");
+        }
+    }
+}
