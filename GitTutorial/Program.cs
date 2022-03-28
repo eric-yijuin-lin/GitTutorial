a8212c52d35736e20a4abb357998c22cfdd6e467
Eric Lin <eric.yijuin.lin@gmail.com>
2022-03-28 19:13:28 +0800

    squash commit 1

--- GitTutorial/Program.cs
+++ GitTutorial/Program.cs
@@ -5,3 +5,5 @@ Console.WriteLine("Modified in Folder B");
 Console.WriteLine("Modified in Folder A");
 Console.WriteLine("Keep going");
 Console.WriteLine("Modified in master branch");
+
+Console.WriteLine("Hi, I'm from squash branch");
