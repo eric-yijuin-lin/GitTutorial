8f344d442102dfb2e5015018c280ae8734f59e39
Eric Lin <eric.yijuin.lin@gmail.com>
2022-03-28 19:21:38 +0800

    Squashed commit of the following:
    commit 026351fc3ef9b7dcc31ab30c1f0871bde201d96f
    Author: Eric Lin <eric.yijuin.lin@gmail.com>
    Date:   Mon Mar 28 19:15:20 2022 +0800
    
        squash commit 3
    
    commit 621599f8a49a99dcb398a3a181095167827265bd
    Author: Eric Lin <eric.yijuin.lin@gmail.com>
    Date:   Mon Mar 28 19:14:32 2022 +0800
    
        squash commit 2
    
    commit a8212c52d35736e20a4abb357998c22cfdd6e467
    Author: Eric Lin <eric.yijuin.lin@gmail.com>
    Date:   Mon Mar 28 19:13:28 2022 +0800
    
        squash commit 1

--- GitTutorial/Program.cs
+++ GitTutorial/Program.cs
@@ -5,3 +5,7 @@ Console.WriteLine("Modified in Folder B");
 Console.WriteLine("Modified in Folder A");
 Console.WriteLine("Keep going");
 Console.WriteLine("Modified in master branch");
+
+Console.WriteLine("Hi, I'm from squash branch");
+Console.WriteLine("Hey, I'm from squash branch too");
+Console.WriteLine("Wow, I'm also from squash branch");
